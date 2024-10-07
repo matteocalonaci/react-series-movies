5d2035af43a5b046312222563f70f4819ece7b14
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-10-07 18:15:00 +0200

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import AppMovies from "./components/AppMovies";
-import MovieDetails from './components/MovieDetials';
-
+import MovieDetails from "./components/MovieDetials";
+import SerieDetails from './components/SerieDetails';
+import AppHeader from "./components/AppHeader";
+import AppSeries from './components/AppSeries';
+import AppHome from "./components/AppHome";
 
 function App() {
   return (
     <>
-<div className="">
-{/* <AppMovies></AppMovies> */}
-<Routes>
-        <Route path="/movies" element={<AppMovies />} />
-        <Route path="/details" element={<MovieDetails />} />
-      </Routes>
-
-</div>
+      <div className="">
+        <AppHeader></AppHeader>
+        <Routes>
+          <Route path="/" element={<AppHome />} />
+          <Route path="/movies" element={<AppMovies />} />
+          <Route path="/movies/:id" element={<MovieDetails />} />
+          <Route path="/series" element={<AppSeries />} />
+          <Route path="/series/:id" element={<SerieDetails />} />
+        </Routes>
+      </div>
     </>
   );
 }
